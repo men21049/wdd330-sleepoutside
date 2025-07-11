30550509b33cdcdf423242486d28f4c1cfa31bd0
OhayitsFay <atu23001@byui.edu>
2025-07-10 23:03:25 -0500

    product detail page

--- src/js/product.js
+++ src/js/product.js
@@ -1,29 +1,10 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
+const productId = getParam("product");
 
-  const cartItems = getLocalStorage("so-cart");
-  // check if product already exists in cart
-  const existingProduct = cartItems.find((item) => item.Id === product.Id);
-  if (existingProduct) {
-    // if it exists, do not add again
-    return;
-  } else {
-    // if it does not exist, add to cart
-    cartItems.push(product);
-  }
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,3 +22,8 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
